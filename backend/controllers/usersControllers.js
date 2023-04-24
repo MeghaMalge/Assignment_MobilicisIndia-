4d32b631907b4e155d9df7d1f6876cff71cea3dd
Megha Malge <meghamalge226@gmail.com>
2023-04-24 18:23:42 +0530

    completed users controllers

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,10 +1,12 @@
 const HttpError = require("../models/Error");
 const User = require("../models/User");
 
+//case1 = Users which have income lower than $5 USD and have a car of brand “BMW” or “Mercedes”.
 const getCase1Users = async (req, res, next) => {
   let users;
   try {
-    users = await User.find();
+    users = await User.find({ car: ["BMW", "Mercedes"] });
+    users = users.filter((user) => parseFloat(user.income.substring(1)) < 5);
   } catch (error) {
     return next(new HttpError("fetching users failed", 500));
   }
@@ -12,10 +14,12 @@ const getCase1Users = async (req, res, next) => {
   res.status(200).json(users);
 };
 
+//case2 = Male Users which have phone price greater than 10,000.
 const getCase2Users = async (req, res, next) => {
   let users;
   try {
-    users = await User.find();
+    users = await User.find({ gender: "Male" });
+    users = users.filter((user) => parseInt(user.phone_price) > 10000);
   } catch (error) {
     return next(new HttpError("fetching users failed", 500));
   }
@@ -23,10 +27,17 @@ const getCase2Users = async (req, res, next) => {
   res.status(200).json(users);
 };
 
+//case3 = Users whose last name starts with “M” and has a quote character length greater than 15 and email includes his/her last name.
 const getCase3Users = async (req, res, next) => {
   let users;
   try {
-    users = await User.find();
+    users = await User.find({
+      last_name: /^M/,
+      $expr: { $gt: [{ $strLenCP: "$quote" }, 15] },
+    });
+    users = users.filter((user) =>
+      user.email.toLowerCase().includes(user.last_name.toLowerCase())
+    );
   } catch (error) {
     return next(new HttpError("fetching users failed", 500));
   }
@@ -34,10 +45,14 @@ const getCase3Users = async (req, res, next) => {
   res.status(200).json(users);
 };
 
+//case4 = Users which have a car of brand “BMW”, “Mercedes” or “Audi” and whose email does not include any digit.
 const getCase4Users = async (req, res, next) => {
   let users;
   try {
-    users = await User.find();
+    users = await User.find({
+      car: ["BMW", "Mercedes", "Audi"],
+      email: { $not: /[0-9]/ },
+    });
   } catch (error) {
     return next(new HttpError("fetching users failed", 500));
   }
